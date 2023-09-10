29ed0b031ca2d3efeee7c128f9910323df0b7a20
xJeypiii <jpjbriones01@gmail.com>
2023-09-10 19:21:14 +0800

    add files

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,98 @@
+import {useEffect, useRef} from 'react';
+import api from '../../api/axiosConfig';
+import {useParams} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import ReviewForm from '../reviewForm/ReviewForm';
+
+import React from 'react'
+
+const Reviews = ({getMovieData,movie,reviews,setReviews}) => {
+
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(()=>{
+        getMovieData(movieId);
+    },[])
+
+    const addReview = async (e) =>{
+        e.preventDefault();
+
+        const rev = revText.current;
+
+        if(!rev.value)
+            return;
+
+        try
+        {
+            const response = await api.post("/api/v1/movies/" + movieId + "/reviews",{body : rev.value});
+
+            const updatedReviews = [...reviews, rev.value];
+    
+            rev.value = "";
+    
+            setReviews(updatedReviews);
+        }
+        catch(err)
+        {
+            console.error(err);
+        }
+        
+
+
+
+    }
+
+  return (
+    <Container>
+        <Row>
+            <Col><h3>Reviews</h3></Col>
+        </Row>
+        <Row className="mt-2">
+            <Col>
+                <img src={movie?.poster} alt="" />
+            </Col>
+            <Col>
+                {
+                    <>
+                        <Row>
+                            <Col>
+                                <ReviewForm handleSubmit={addReview} revText={revText} labelText = "Write a Review?" />  
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <hr />
+                            </Col>
+                        </Row>
+                    </>
+                }
+                {
+                    reviews?.map((value, index) => {
+                        return(
+                            <div key={index}>
+                                <Row>
+                                    <Col>{value}</Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <hr />
+                                    </Col>
+                                </Row>                                
+                            </div>
+                        )
+                    })
+                }
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <hr />
+            </Col>
+        </Row>        
+    </Container>
+  )
+}
+
+export default Reviews
